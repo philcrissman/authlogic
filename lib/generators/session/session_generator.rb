0038485b49e553c30790655b2756e0b088076df8
Phil Crissman <philcrissman@zeppo.local>
2010-02-20 08:41:38 -0600

    whoops

--- lib/generators/session/session_generator.rb
+++ lib/generators/session/session_generator.rb
@@ -1,7 +1,6 @@
 require 'rails/generators'
 
 class SessionGenerator < Rails::Generators::Base
-  check_class_collision
   
   def create_session_model
     template "session.tt", "app/models/#{file_name}.rb"
