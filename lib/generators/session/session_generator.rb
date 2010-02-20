6a178cbf18c53042ae20e8b4f8dd7b022cd6142f
Phil Crissman <philcrissman@zeppo.local>
2010-02-20 09:05:42 -0600

    fixing the generator

--- lib/generators/session/session_generator.rb
+++ lib/generators/session/session_generator.rb
@@ -3,7 +3,7 @@ require 'rails/generators'
 class SessionGenerator < Rails::Generators::Base
   
   def create_session_model
-    template "session.tt", "app/models/#{file_name}.rb"
+    template "session.tt", "app/models/#{name}.rb"
   end
   
 end
